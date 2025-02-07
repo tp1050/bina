c46743aaf16ca99d3f024f0d2f1f327a2ad80340
tp1050 <tp1050@gmail.com>
2025-02-07 19:26:22 +0330

    ss

--- easy/main.py
+++ easy/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template,  redirect,request
-from zto4.extraction.barcode import  get_product_by_barcode
+from zto4.extraction.barcode2 import  get_product_by_barcode
 import json
 import pandas as pd
 from functools import lru_cache
