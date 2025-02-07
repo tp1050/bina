b739c6e130be40150fef8d5ef085ee35e2f3dcd6
tp1050 <tp1050@gmail.com>
2025-02-08 00:49:59 +0330

    s

--- data_maintaince/process_gtins.py
+++ data_maintaince/process_gtins.py
@@ -4,18 +4,26 @@ import json
 from pathlib import Path as P
 from zto4.coms.net import fetch
 from zto4.search.google.lib import google_barcode_serperdev
-
-from torch import gt
+from zto4.capitals.lib import SAVE,DOMAIN
+from datetime import datetime
 gtin_log_path='/tmp/accepter/gtin/gtin.log'
 
 
 def google_gtin_logs():
+
     gtins=sorted(
             list(set(P(gtin_log_path).read_text().split('\n'))
                  )
             )
-    results=[google_barcode_serperdev(barcode=g) for g in gtins:
-        google_barcode_serperdev
+    links=[]
+    for g in gtins:
+        links.extend(google_barcode_serperdev(barcode=g))
+    htmls=[]
+    for l in links:
+        html=fetch(l,return_type="source")
+        SAVE(obj=html,where=f'/home/c/.zto/assets/bina/sku/gserp_fetch/{DOMAIN(l)}_{g}_{datetime.now().ctime()}.html')
+        htmls.append(html)
+    
     
             
     
