4e83ee495710284078c4aa45679195f615107596
tp1050 <tp1050@gmail.com>
2025-02-07 13:39:36 +0330

    A

--- easy/test.sh
+++ easy/test.sh
@@ -1,4 +1,6 @@
-source /opt/accepter/venv/bin/activate
+cd  /opt/accepter/venv/site-packages/zto4
 gh repo sync
-
+cd ~/code/bina
+gh repo sync
+source /opt/accepter/venv/bin/activate
 python3 main.py
